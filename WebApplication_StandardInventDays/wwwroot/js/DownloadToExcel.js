f69b3d1969d04c9fd7155aef9538ba113716460f
3121500003@msoffice.pens.ac.id <3121500003@msoffice.pens.ac.id>
2023-10-19 13:30:37 +0700

    belumdikasihcrud

---
+++ WebApplication_StandardInventDays/wwwroot/js/DownloadToExcel.js
@@ -0,0 +1,60 @@
+﻿// Fungsi untuk menambah ukuran font
+function increaseFontSize() {
+    changeFontSize(2); // Mengatur penambahan 2px
+}
+
+// Fungsi untuk mengurangi ukuran font
+function decreaseFontSize() {
+    changeFontSize(-2); // Mengatur pengurangan 2px
+}
+
+// Fungsi umum untuk mengubah ukuran font pada elemen th dan td
+function changeFontSize(changeAmount) {
+    const tableCells = document.querySelectorAll('.ViewExcel td, .ViewExcel th');
+
+    tableCells.forEach(cell => {
+        let currentFontSize = parseFloat(window.getComputedStyle(cell).fontSize);
+        let newFontSize = currentFontSize + changeAmount;
+
+        // Batas ukuran font minimal (8px) dan maksimal (20px)
+        if (newFontSize < 8) {
+            newFontSize = 8;
+        } else if (newFontSize > 20) {
+            newFontSize = 20;
+        }
+
+        cell.style.fontSize = newFontSize + 'px';
+    });
+}
+
+// Menambahkan event listener ke tombol plus dan minus
+document.getElementById('increase-text-size').addEventListener('click', increaseFontSize);
+document.getElementById('decrease-text-size').addEventListener('click', decreaseFontSize);
+
+
+
+
+
+
+// POPUP 
+// Function to open the popup
+document.getElementById('open-popup').addEventListener('click', function () {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block'; // Menampilkan overlay
+});
+
+// Function to close the popup and hide the overlay
+document.getElementById('close-popup').addEventListener('click', function () {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none'; // Menyembunyikan overlay
+});
+
+// Function to trigger download based on selected format
+document.getElementById('download-button').addEventListener('click', function () {
+    console.log('Download button clicked');
+    const selectedFormat = document.getElementById('file-format').value;
+    alert(`Downloading file in format: ${selectedFormat}`);
+    // Setelah unduhan selesai, tutup popup dan sembunyikan overlay
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+});
--- WebApplication_StandardInventDays/wwwroot/js/site.js
+++ WebApplication_StandardInventDays/wwwroot/js/site.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
 });
 
 
-
+// sidebar menu collapse
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('.sidebar .nav-link').forEach(function (element) {
 
@@ -62,3 +62,228 @@ document.addEventListener("DOMContentLoaded", function () {
     }) // forEach
 });
 // DOMContentLoaded  end
+// End Sidebar menu collapse
+
+
+
+// Form Ouput Hasil
+$("#processButton").click(function (e) {
+    e.preventDefault(); 
+    var hasil = "120"; 
+    $("#hasil").val(hasil);
+});
+
+$("#clearButton").click(function (e) {
+    e.preventDefault(); 
+    $("form")[0].reset(); 
+    $("#hasil").val(""); 
+});
+
+// End Output Hasil
+
+
+// Grafik Chart 1
+var dataBar = {
+    labels: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
+    datasets: [
+        {
+            label: "Data 1",
+            backgroundColor: "#4EA652", // Warna hijau dengan tingkat transparansi
+            data: [30, 25, 5, 10, 15, 18, 22, 14, 25, 25, 20, 28]
+        },
+        {
+            label: "Data 2",
+            backgroundColor: "#A50808", // Warna merah dengan tingkat transparansi
+            data: [50, 25, 55, 30, 10, 20, 15, 25, 28, 22, 18, 28]
+        },
+        {
+            label: "Data 3",
+            backgroundColor: "#004891", // Warna biru dengan tingkat transparansi
+            data: [30, 35, 45, 25, 18, 25, 20, 28, 15, 22, 28, 12]
+        },
+        {
+            label: "Data 4",
+            backgroundColor: "#FFD600", // Warna biru dengan tingkat transparansi
+            data: [25, 30, 48, 30, 18, 25, 20, 27, 15, 22, 28, 12]
+        },
+        {
+            label: "Data 5",
+            type: "line",
+            borderColor: "rgba(0, 0, 0, 1)", // Warna garis hitam
+            borderWidth: 1, // Lebar garis
+            data: [30, 55, 45, 60, 25, 10, 5, 26, 28, 30, 20, 39]
+        }
+    ]
+};
+
+// Opsi konfigurasi grafik
+var options = {
+    scales: {
+        x: {
+            beginAtZero: true
+        },
+        y: {
+            beginAtZero: true
+        }
+    }
+};
+
+// Mengambil elemen canvas dan membuat grafik tipe bar
+var ctxBar = document.getElementById("grafik1").getContext("2d");
+var myChartBar = new Chart(ctxBar, {
+    type: "bar",
+    data: dataBar,
+    options: options
+});
+// End Grafik Chart 1
+
+
+// Grafik Chart 2
+// Data untuk grafik tipe donat (grafik 2)
+var dataDonut = {
+    labels: ["Label 1", "Label 2", "Label 3", "Label 4"],
+    datasets: [
+        {
+            data: [30, 40, 20, 15],
+            backgroundColor: ["#4EA652", "#A50808", "#004891", "#FFD600"]
+        }
+    ]
+};
+
+// function options chart 
+var optionsDonut = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+            position: 'bottom' // Menempatkan legenda di bawah grafik
+        }
+    }
+};
+
+// Mengambil elemen canvas dan membuat grafik tipe donat (grafik 2)
+var ctxDonut = document.getElementById("grafik2").getContext("2d");
+var myChartDonut = new Chart(ctxDonut, {
+    type: "doughnut",
+    data: dataDonut,
+    options: optionsDonut
+});
+// End Grafik Chart 2
+
+
+// Dropwon Chart
+$(document).ready(function () {
+    // Ketika item dropdown dipilih
+    $(".dropdown-item").click(function () {
+        // Hapus kelas active dari semua item dropdown
+        $(".dropdown-item").removeClass("active");
+
+        // Tambahkan kelas active ke item yang dipilih
+        $(this).addClass("active");
+
+        // Ambil nilai (value) dari item yang dipilih
+        var selectedValue = $(this).attr("value");
+
+        // Ganti teks pada tombol dropdown dengan nilai yang dipilih
+        $(".btn-typeChart").text(selectedValue);
+
+        // Anda juga dapat melakukan tindakan lain sesuai kebutuhan di sini
+
+        // Contoh: Cetak nilai yang dipilih ke konsol
+        console.log("Anda memilih: " + selectedValue);
+    });
+});
+// End Dropdown Chart
+
+
+// Progress Bar Chart Atas 
+// Mengambil elemen-elemen progress bar berdasarkan class mereka
+var progressBarIMP = document.querySelector(".progress-barIMP");
+var progressBarLOC = document.querySelector(".progress-barLOC");
+var progressBarALL = document.querySelector(".progress-barALL");
+
+// Mengatur nilai progress bar untuk masing-masing elemen
+var progressValueIMP = 32; // Nilai untuk progress bar IMP
+var progressValueLOC = 65; // Nilai untuk progress bar LOC
+var progressValueALL = 80; // Nilai untuk progress bar ALL
+
+// Mengatur nilai atribut aria-valuenow dan lebar progress bar
+progressBarIMP.style.width = progressValueIMP + "%";
+progressBarIMP.setAttribute("aria-valuenow", progressValueIMP);
+
+progressBarLOC.style.width = progressValueLOC + "%";
+progressBarLOC.setAttribute("aria-valuenow", progressValueLOC);
+
+progressBarALL.style.width = progressValueALL + "%";
+progressBarALL.setAttribute("aria-valuenow", progressValueALL);
+
+// End Progress Bar Chart Atas
+
+
+// Donut Chart Bawah
+const progressCircles = document.querySelectorAll('.progress-circle');
+
+// Contoh data dari sumber data (bisa diganti dengan data dari database/API)
+const dataFromDatabase = [
+    { value: 40, color: '#3498db' },
+    { value: 65, color: '#e74c3c' },
+    { value: 20, color: '#27ae60' },
+    { value: 75, color: '#27ae60' },
+];
+
+function updateProgress() {
+    dataFromDatabase.forEach((data, index) => {
+        const circle = progressCircles[index];
+        const currentValue = parseInt(circle.querySelector('.progress-label').textContent);
+        const targetValue = data.value;
+
+        if (currentValue < targetValue) {
+            circle.querySelector('.progress-label').textContent = `${currentValue + 1}%`;
+            circle.style.transform = `rotate(${(currentValue + 1) * 3.6}deg)`;
+        }
+    });
+}
+
+// Memulai animasi otomatis saat halaman dimuat
+setInterval(updateProgress, 50);
+
+// End Donut Chart Bawah
+
+
+
+$(document).ready(function () {
+    $('.dropdown-toggle').dropdown(); // Menginisialisasi perilaku dropdown pada elemen dengan class "dropdown-toggle"
+});
+
+
+
+
+
+
+
+ // Dark Mode 
+// Mengambil elemen tombol switch
+//const darkModeToggle = document.getElementById("dark-mode-toggle");
+
+//// Memeriksa apakah mode gelap sudah diaktifkan sebelumnya
+//if (localStorage.getItem("dark-mode") === "enabled") {
+//    document.body.classList.add("dark-mode");
+//    darkModeToggle.checked = true;
+//}
+
+//// Fungsi untuk mengaktifkan atau menonaktifkan mode gelap
+//function toggleDarkMode() {
+//    if (document.body.classList.contains("dark-mode")) {
+//        document.body.classList.remove("dark-mode");
+//        localStorage.setItem("dark-mode", "disabled");
+//    } else {
+//        document.body.classList.add("dark-mode");
+//        localStorage.setItem("dark-mode", "enabled");
+//    }
+//}
+
+//// Mendengarkan perubahan pada tombol switch
+//darkModeToggle.addEventListener("change", toggleDarkMode);
+
+
+
